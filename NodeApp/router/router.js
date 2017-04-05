3625ead59395992fee019c01bf0fefe275893ea4
Angel-Minguez <sindariel.eq@gmail.com>
2017-04-05 15:14:48 +0200

    v.0.0.3

--- NodeApp/router/router.js
+++ NodeApp/router/router.js
@@ -2,11 +2,14 @@
 /*				        Modulo router                         */
 /**************************************************************/
 //Importamos la instancia de Express
-var app = require('../server.js');
+var app = require('../server.js').app;
 //Cargamos el controlador
 var controller = require('../controller/controller.js');
 //Objeto router
 module.exports = function (req, res, next) {
-    app.use('/', controller.userLogin);
+    app.get('/', controller.userLogin);
+    app.post('/form1', (req, res) => {
+        console.log(req.body.campo1);
+    });
 	next(); //Invocamos el siguiente elemento de la pila de middleware  
 }
--- NodeApp/server.js
+++ NodeApp/server.js
@@ -2,15 +2,18 @@
 /*				        Modulo principal                      */
 /**************************************************************/
 'use strict';
-//Inicializacion de Express
+//Inicializacion de Express y otros modulos
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 //Exportamos la instancia de nuestra aplicacion
-module.exports = app;
-//Requerimos y añadimos nuestro router a la pila de middleware
+module.exports.app = app;
 //Caso de dependencia circular, router necesita que app haya sido exportada
-var router = require('./router/router.js');
-app.use(router);
+//Requerimos y añadimos nuestro router a la pila de middleware
+var appRouter = require('./router/router.js');
+app.use(appRouter);
+app.use(bodyParser.json());                         //Parseador en formato json
+app.use(bodyParser.urlencoded({extended:true}));    //Parseador de parametros codificados en url
 app.listen(process.env.PORT);
 //Servidor de archivos estaticos
 app.use(express.static('static'));

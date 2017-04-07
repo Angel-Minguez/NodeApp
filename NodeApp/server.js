e3d428d6aea2aee45691ff1d3e34c5439b2d2b22
Angel-Minguez <sindariel.eq@gmail.com>
2017-04-07 15:22:08 +0200

    v.0.0.5

--- NodeApp/server.js
+++ NodeApp/server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const sessions = require('express-session');
-const mongoStore = require('connect-mongo');
+const mongoStore = require('connect-mongo')(sessions); 	//Asociamos controlador y almacenamiento
 //Exportamos la instancia de nuestra aplicacion
 module.exports.app = app;
 //Caso de dependencia circular, router necesita que app haya sido exportada
@@ -16,28 +16,25 @@ var router = require('./router/router.js');
 app.use(router);
 app.use(bodyParser.json());                         //Parseador en formato json
 app.use(bodyParser.urlencoded({extended:true}));    //Parseador de parametros codificados en url
-mongoStore(sessions);								//Asociamos controlador y almacenamiento
-app.use(sessions(global.sessionOptions));			//Controlador de sesiones
 //Opciones de la sesion
-if (process.env.NODE_ENV === 'developement')
-	global.sessionOptions = {secret: 'mi secreto', 
-							 store: new mongoStore(),
-							 saveUninitialized: false, 
-							 cookie:{
-								 domain: '127.0.0.1', 
-								 maxAge: 1000*60*60
-								 }
-							};
-else global.sessionOptions = {
-							secret: 'mi secreto', 
-							cookie:{
-								secure : true
-								}
-							};
+var sessionOptions = {};
+if (app.get('env') === 'developement') sessionOptions = {
+                                                          secret: 'mi secreto', 
+                                                          store: new mongoStore({ url:'mongodb://127.0.0.1/nodeApp'}),
+							                              saveUninitialized: false, 
+                                                          cookie: {
+								                                domain: '127.0.0.1', 
+								                                maxAge: 1000*60*60 }
+							                            };
+else sessionOptions = {
+						secret: 'mi secreto', 
+						cookie: {
+							secure : true }
+};
+app.use(sessions(sessionOptions));			        //Controlador de sesiones
 //Servidor de archivos estaticos
 app.use(express.static('static'));
 //Seleccion del modulo de plantillas
 app.set('views', './views'); //Ruta a la carpeta de los templates
 app.set('view engine', 'pug'); //Motor que usaremos
 app.listen(process.env.PORT);
-//http://stackoverflow.com/questions/26626249/update-part-of-a-page-in-jade

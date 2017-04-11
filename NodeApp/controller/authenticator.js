c38d8f31b6e8be9f8d0b5cc6729e697dc4e4844f
Angel-Minguez <sindariel.eq@gmail.com>
2017-04-11 18:33:49 +0200

    v.0.1.0

---
+++ NodeApp/controller/authenticator.js
@@ -0,0 +1,14 @@
+﻿/**************************************************************/
+/*				      Modulo autenticador                     */
+/**************************************************************/
+'use strict'
+module.exports = function(req, res, next) {
+	console.log('aut:',req.session.user);
+	console.log('aut:',req.url);
+	if (!req.session.user && req.url !== '/login'&& req.url !== '/login_form') {
+		req.session.user = 'guest';
+		res.redirect('/login');	
+	}
+	else if (req.session.user === 'guest' && req.url !== '/login' && req.url !== '/login_form') res.redirect('/login');
+		else next()
+}
